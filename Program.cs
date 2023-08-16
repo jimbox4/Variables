256905e73cb249bad6e271359a64c2917b564640
Alexandr <jimbox2001@mail.ru>
2023-08-16 15:08:01 +0300

    Update Program.cs
    Making 10 types of variables.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int maxHealth;
+int currentHealth;
+
+string characterName;
+
+bool isHungry;
+
+float characterPositionX;
+float characterPositionY;
+
+ulong characterScore;
+
+double worldPosition;
+
+byte characterMoney;
+
+sbyte studentsSouls;
+
+long moneyDebt;
+
+uint hometasks;
